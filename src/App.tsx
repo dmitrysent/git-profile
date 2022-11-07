a10c65678494736e405c2d4d751802347653fa11
Dmitry Sentyurev <dmitry.sent@gmail.com>
2022-11-07 21:35:31 +0300

    Добавил компоненты контейнера, header, переключатель темы

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import {Container} from "./components/Container";
+import {ThemeSwitcher} from "./components/ThemeSwitcher";
+import {TheHeader} from "./components/TheHeader";
+
 function App() {
   return (
-    <>
-      
-    </>
+    <Container>
+      {<TheHeader />}
+    </Container>
   );
 }
 
